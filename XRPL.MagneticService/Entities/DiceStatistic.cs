68812ab29dc9bc43a2652e02d492f1ac198e6aa6
Aleksandr <platonenkov87@gmail.com>
2023-03-12 14:27:44 +0300

    add new statistics point

---
+++ XRPL.MagneticService/Entities/DiceStatistic.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XRPL.MagneticService.Entities
+{
+    public class DiceStatistic
+    {
+        public string Wallet { get; set; }
+        public string Code { get; set; }
+        public string Issuer { get; set; }
+        public uint TotalGames { get; set; }
+        public uint TotalWins { get; set; }
+        public uint TotalLoss { get; set; }
+        public double LossBlanace { get; set; }
+        public double WinBlanace { get; set; }
+        public double TotalVolume { get; set; }
+    }
+}
